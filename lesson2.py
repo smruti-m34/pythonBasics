80614d4fc7fc681ae736f0032d3ce5f8d464f469
smruti-m34 <smrutimallick34@gmail.com>
2025-07-25 16:42:40 +0530

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+name= input("what is your name:  ")
+age=int(input("what is your age:  "))
+place=input("where do you live:   ")
+print(f"My name is {name}\n I'm {age} years old\n i live in {place}")
+
+import keyword
+print(keyword.kwlist)
